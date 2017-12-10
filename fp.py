e4a9ff0a212c5ecb98f691462b9aac587f62fe47
jvandalsem <jjvandal@umich.edu>
2017-12-10 14:37:32 -0500

    Updating fp.py included sqlite

---
+++ fp.py
@@ -0,0 +1,8 @@
+import requests
+import json
+import urllib
+
+# GroupMe (wrapper), Facebook, Instagram, Spotify, Github
+# Grab day of week for each interaction and time of day
+# Cache ---> must work with cache (turn internet off), databases (look at datapoints)
+# Just need to submit cache and databases
